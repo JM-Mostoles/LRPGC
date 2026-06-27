0107bb4d896799ceb828d999375609ab6afab7b6
Julio <juliomontero.mostoles@gmail.com>
2026-06-27 18:12:09 +0200

    Animation library

--- libraries/FacilAnims.lua
+++ libraries/FacilAnims.lua
@@ -1,34 +1,43 @@
 local facilAnims = {}
 
-function facilAnims.new(image, iWidth, iHeight, hFrames, vFrames, speed, X, Y, oX, oY)
+function facilAnims.new(image, hFrames, vFrames, speed, X, Y, oX, oY, from, to)
     local newAnimationObject = {}
 
     newAnimationObject.quads = {}
-
-    newAnimationObject.quads.currentFrame = 1
+    newAnimationObject.index = 1
+    newAnimationObject.currentFrame = newAnimationObject.quads[newAnimationObject.index]
+    newAnimationObject.timer = 1
 
     for i = 1, hFrames do
         for j = 1, vFrames do
             table.insert(newAnimationObject.quads,
                 love.graphics.newQuad(
-                    ((j - 1) * (iWidth / hFrames)),
-                    ((i - 1) * (iHeight / vFrames)),
-                    (iWidth / hFrames),
-                    (iHeight / vFrames),
-                    iWidth,
-                    iHeight
+                    ((j - 1) * (image:getWidth()  / hFrames)),
+                    ((i - 1) * (image:getHeight()  / vFrames)),
+                    (image:getWidth() / hFrames),
+                    (image:getHeight() / vFrames),
+                    image:getWidth() ,
+                    image:getHeight()
                 )
             )
         end
     end
+    newAnimationObject.index = 1
+    newAnimationObject.currentFrame = newAnimationObject.quads[newAnimationObject.index]
 
     function newAnimationObject:update(dt)
-        self.currentFrame = self.currentFrame + (speed * dt)
-        
+        newAnimationObject.timer = (newAnimationObject.timer + (speed * dt))
+        newAnimationObject.index = math.floor(newAnimationObject.timer)
+        newAnimationObject.currentFrame = newAnimationObject.quads[newAnimationObject.index]
+
+        if newAnimationObject.index == #newAnimationObject.quads then
+            newAnimationObject.timer = 1
+            newAnimationObject.index = 1
+        end
     end
 
     function newAnimationObject:play()
-        love.graphics.draw(image, self.currentFrame, self.X, self.Y, nil, 1, 1, self.oX. self.oY)
+        love.graphics.draw(image, newAnimationObject.currentFrame, X, Y, 0, 1, 1, oX, oY)
     end
 
     return newAnimationObject
--- main.lua
+++ main.lua
@@ -1,23 +1,16 @@
-local world
-local FacilPlayer = require("libraries.FacilPlayer")
-local FacilObject = require("libraries.FacilObject")
+local facilanims = require("libraries.FacilAnims")
 function love.load()
-  world = love.physics.newWorld(0, 0, true)
-  facildidy = FacilPlayer.new(world, 90, 90, 20, 20, 100)
-  obj1 = FacilObject.new(world, 20, 40, 10, 30)
-  obj2 = FacilObject.new(world, 40, 140, 50, 40)
+  image = love.graphics.newImage("rs/gub.png")
+  cof = facilanims.new(image, 2, 2, 10, 20, 20, 10, 10)
 end
 
 function love.update(dt)
-  facildidy:update(dt)
-  world:update(dt)
+  cof:update(dt)
 end
 
 function love.keypressed(key)
 end
 
 function love.draw()
-  facildidy:draw()
-  obj1:draw()
-  obj2:draw()
+  cof:play()
 end
